89c01f1fdde6309f5735f0ad02a3f9531f06d86c
allisonjungg <allisonjungg@gmail.com>
2018-07-13 21:01:22 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,4 +4,6 @@ function h1Selector(){
   return $('h1') //short cut
   //return document.querySelector('h1') //long hand
 }
-function liInO1Selector
+function liInO1Selector(){
+  return $("o1>li")
+}
